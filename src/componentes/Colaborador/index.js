b0122b254ee14dc76ece6f6c4e5d432c95d2c6d0
Mariane Limoli <marianelimoli@gmail.com>
2023-10-19 10:46:25 -0300

    add color

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,15 +1,20 @@
-import './Colaborador.css'
+import { AiFillCloseCircle } from "react-icons/ai";
 
-const Colaborador = ({ nome, imagem, cargo, corDeFundo }) => {
-    return (<div className='colaborador'>
-        <div className='cabecalho' style={{ backgroundColor: corDeFundo }}>
-            <img src={imagem} alt={nome}/>
-        </div>
-        <div className='rodape'>
-            <h4>{nome}</h4>
-            <h5>{cargo}</h5>
-        </div>
-    </div>)
-}
+import "./Colaborador.css";
 
-export default Colaborador
+const Colaborador = ({ nome, imagem, cargo, corDeFundo, aoDeletar }) => {
+  return (
+    <div className="colaborador">
+      <AiFillCloseCircle size={25} className="deletar" onClick={aoDeletar} />
+      <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+        <img src={imagem} alt={nome} />
+      </div>
+      <div className="rodape">
+        <h4>{nome}</h4>
+        <h5>{cargo}</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Colaborador;
--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,18 +1,32 @@
-import Colaborador from '../Colaborador'
-import './Time.css'
+import Colaborador from "../Colaborador";
 
-const Time = (props) => {
-    const css = { backgroundColor: props.corSecundaria }
+import "./Time.css";
 
-    return (
-        (props.colaboradores.length > 0) ? <section className='time' style={css}>
-            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-            <div className='colaboradores'>
-                {props.colaboradores.map( colaborador => <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/> )}
-            </div>
-        </section> 
-        : ''
-    )
-}
+const Time = (props, aoDeletar) => {
+  const css = { backgroundColor: props.corSecundaria };
 
-export default Time
+  return props.colaboradores.length > 0 ? (
+    <section className="time" style={css}>
+      <input value={props.corSecundaria} type="color" className="input-cor" />
+      <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+      <div className="colaboradores">
+        {props.colaboradores.map((colaborador) => {
+          return (
+            <Colaborador
+              corDeFundo={props.corPrimaria}
+              key={colaborador.nome}
+              nome={colaborador.nome}
+              cargo={colaborador.cargo}
+              imagem={colaborador.imagem}
+              aoDeletar={aoDeletar}
+            />
+          );
+        })}
+      </div>
+    </section>
+  ) : (
+    ""
+  );
+};
+
+export default Time;
